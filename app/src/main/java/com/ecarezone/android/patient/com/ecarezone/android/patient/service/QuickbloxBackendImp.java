0ba7537dea2f90030015275224fc9ea655cdfe00
Chao Wei <chao.wei@aalto.fi>
2015-05-17 19:15:13 +0300

    update graphics assets, and registration interface

---
+++ app/src/main/java/com/ecarezone/android/patient/com/ecarezone/android/patient/service/QuickbloxBackendImp.java
@@ -0,0 +1,8 @@
+package com.ecarezone.android.patient.com.ecarezone.android.patient.service;
+
+/**
+ * Created by CHAO WEI on 5/17/2015.
+ */
+class QuickbloxBackendImp {
+
+}
---
+++ app/src/main/java/com/ecarezone/android/patient/com/ecarezone/android/patient/service/WebService.java
@@ -0,0 +1,63 @@
+package com.ecarezone.android.patient.com.ecarezone.android.patient.service;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import org.json.JSONObject;
+
+
+/**
+ * Created by CHAO WEI on 5/17/2015.
+ */
+public class WebService {
+
+    public String getCallerName() {
+        return  WebService.class.getSimpleName();
+    }
+
+    private static final String UTF_8 =  "UTF-8";
+
+    /**
+     * WebService singleton instance
+     */
+    private static WebService sInstance;
+    private Context mContext = null;
+    private Resources mResources = null;
+
+    /**
+     * Get the {@link WebService} instance.
+     *
+     * @param context
+     *            Application context.
+     *
+     * @return {@link WebService} instance.
+     */
+    public static WebService getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new WebService(context);
+        }
+        return sInstance;
+    }
+
+    private WebService(Context context) {
+        mContext = context;
+        mResources = context.getResources();
+    }
+
+
+    public JSONObject register() {
+
+        return  null;
+    }
+
+    public JSONObject login() {
+
+        return null;
+    }
+
+    public JSONObject lgout() {
+
+        return  null;
+    }
+
+}
