fcb0ff1a13b540252e45660f2e3a7440ef9297af
Chao Wei <chao.wei@aalto.fi>
2015-06-01 00:06:53 +0300

    user profile details ui

--- app/src/main/java/com/ecarezone/android/patient/ProfileDetailsActivity.java
+++ app/src/main/java/com/ecarezone/android/patient/ProfileDetailsActivity.java
@@ -1,14 +1,21 @@
 package com.ecarezone.android.patient;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.ecarezone.android.patient.fragment.UserProfileDetailsFragment;
-import com.ecarezone.android.patient.fragment.UserProfileFragment;
 
 /**
  * Created by CHAO WEI on 5/31/2015.
  */
 public class ProfileDetailsActivity extends EcareZoneBaseActivity {
+
+    private ActionBar mActionBar = null;
+    private Toolbar mToolBar = null;
+
     @Override
     protected String getCallerName() {
         return ProfileDetailsActivity.class.getSimpleName();
@@ -19,7 +26,25 @@ public class ProfileDetailsActivity extends EcareZoneBaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_profile);
         onNavigationChanged(R.layout.frag_user_profle_details, null);
+        mToolBar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        if (mToolBar != null) {
+            setSupportActionBar(mToolBar);
+            mToolBar.setNavigationIcon(R.drawable.ic_action_menu);
+        }
+        mActionBar = getSupportActionBar();
+        mActionBar.setHomeButtonEnabled(true);
+        mActionBar.setDisplayHomeAsUpEnabled(true);
         addSupportOnBackStackChangedListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            popBackStack();
+            //finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
