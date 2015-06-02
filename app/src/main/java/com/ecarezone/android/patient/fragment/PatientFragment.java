ca3bcf01136ece35c0d241985cb714105143fbb6
Chao Wei <chao.wei@aalto.fi>
2015-06-03 02:00:30 +0300

    home UI

--- app/src/main/java/com/ecarezone/android/patient/fragment/PatientFragment.java
+++ app/src/main/java/com/ecarezone/android/patient/fragment/PatientFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ecarezone.android.patient.R;
 
@@ -19,6 +20,7 @@ public class PatientFragment extends EcareZoneBaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.frag_patient_main, container, false);
+        ((TextView) view.findViewById(R.id.text_view_home_message_value)).setText(String.format(getString(R.string.home_new_messages), getString(R.string.doctor_name1)));
         return view;
     }
 
