b0336d902faebf184f2723c621c5cac9f2097e88
Satish Megadri <satish.megadri@lnttechservices.com>
2016-03-11 18:48:07 +0530

    Chat History implementation: 1.chat database design 2.storing data in database 3.show data in UI after saving. For Now ecareuser@mail.com is the static user whom you can sent chat.For getting message you have to login with another device with username 'ecareuser@mail.com" password '12345678'

--- app/src/main/java/com/ecarezone/android/patient/utils/SinchUtil.java
+++ app/src/main/java/com/ecarezone/android/patient/utils/SinchUtil.java
@@ -1,9 +1,13 @@
 package com.ecarezone.android.patient.utils;
 
-import android.content.Context;
+        import android.content.Context;
 
-import com.ecarezone.android.patient.app.AudioPlayer;
-import com.ecarezone.android.patient.service.SinchService;
+        import com.ecarezone.android.patient.app.AudioPlayer;
+        import com.ecarezone.android.patient.config.Constants;
+        import com.ecarezone.android.patient.model.Chat;
+        import com.ecarezone.android.patient.model.database.ChatDbApi;
+        import com.ecarezone.android.patient.service.SinchService;
+        import com.sinch.android.rtc.messaging.Message;
 
 /**
  * Created by L&T Technology Services on 2/17/2016.
@@ -26,5 +30,20 @@ public class SinchUtil {
 
     public static void setSinchAudioPlayer(Context context) {
         audioplayer = new AudioPlayer(context);
+    }
+
+    public static void saveIncomingChatHistory(Message message, Context context) {
+        Chat chat = new Chat();
+        if (message.getTextBody().contains(Constants.ENDPOINTURL)) {
+            chat.setInComingImageUrl(message.getTextBody());
+        } else {
+            chat.setMessageText(message.getTextBody());
+        }
+        chat.setTimeStamp(message.getTimestamp());
+        chat.setChatUserId(message.getSenderId());
+        chat.setChatType(ChatDbApi.CHAT_INCOMING);
+        chat.setReadStatus(ChatDbApi.CHAT_UNREAD_STATUS);
+
+        ChatDbApi.getInstance(context).saveChat(chat);
     }
 }
